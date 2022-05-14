36f67249da452ee74beb01eb61c70f7427bb661d
MikeGulik <mgulik@grubbrr.com>
2022-05-14 15:00:34 -0400

    added second web project for test

---
+++ Tauri.Web/Tauri.Web/SimpleApiController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Tauri.Web
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SimpleApiController : ControllerBase
+    {
+        // GET: api/<SimpleApiController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<SimpleApiController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SimpleApiController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SimpleApiController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SimpleApiController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- tauri-app/src-tauri/src/main.rs
+++ tauri-app/src-tauri/src/main.rs
@@ -12,9 +12,42 @@ async fn write_report(app: tauri::AppHandle) -> Result<(), String> {
   Ok(())
 }
 
-fn main() {
-  tauri::Builder::default()
-  .invoke_handler(tauri::generate_handler![write_report])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+// fn main() {
+//   tauri::Builder::default()
+//   .invoke_handler(tauri::generate_handler![write_report])
+//     .run(tauri::generate_context!())
+//     .expect("error while running tauri application");
+// }
+
+
+fn main() -> wry::Result<()> {
+  use wry::{
+    application::{
+      event::{Event, StartCause, WindowEvent},
+      event_loop::{ControlFlow, EventLoop},
+      window::WindowBuilder,
+    },
+    webview::WebViewBuilder,
+  };
+
+  let event_loop = EventLoop::new();
+  let window = WindowBuilder::new()
+    .with_title("Hello World")
+    .build(&event_loop)?;
+  let _webview = WebViewBuilder::new(window)?
+    .with_url("https://localhost:7090")?
+    .build()?;
+
+  event_loop.run(move |event, _, control_flow| {
+    *control_flow = ControlFlow::Wait;
+
+    match event {
+      Event::NewEvents(StartCause::Init) => println!("Wry has started!"),
+      Event::WindowEvent {
+        event: WindowEvent::CloseRequested,
+        ..
+      } => *control_flow = ControlFlow::Exit,
+      _ => (),
+    }
+  });
 }
